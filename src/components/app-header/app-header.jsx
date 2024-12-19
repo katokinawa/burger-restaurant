21090e1401f67955b9a2b04cfbd3cb044d0928e5
Daniil Belogur <katokinawa@icloud.com>
2024-12-19 20:59:44 +0300

    feat: AppHeader is done

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,22 +1,42 @@
 // import { useState } from 'react'
 import styles from "./app-header.module.css";
-import { BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import {
+  BurgerIcon,
+  ListIcon,
+  ProfileIcon,
+  Logo,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 
 export default function AppHeader() {
   return (
-    <section className={styles.header}>
-      <button className={styles.button}>
-        <BurgerIcon type="primary" />
-        <p className={styles.text}>Конструктор</p>
-      </button>
-      <button className={styles.button}>
-        <ListIcon type="primary" />
-        <p className={styles.text}>Лента заказов</p>
-      </button>
-      <button className={styles.button}>
-        <ProfileIcon type="primary" />
-        <p className={styles.text}>Личный кабинет</p>
-      </button>
-    </section>
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <ul className={styles.nav_left}>
+          <li>
+            <button className={styles.button}>
+              <BurgerIcon type="primary" />
+              <p className="text text_type_main-default">Конструктор</p>
+            </button>
+          </li>
+          <li>
+            <button className={styles.button}>
+              <ListIcon type="primary" />
+              <p className="text text_type_main-default">Лента заказов</p>
+            </button>
+          </li>
+        </ul>
+        <div className={styles.nav_center}>
+          <Logo />
+        </div>
+        <ul className={styles.nav_right}>
+          <li>
+            <button className={styles.button}>
+              <ProfileIcon type="primary" />
+              <p className="text text_type_main-default">Личный кабинет</p>
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
