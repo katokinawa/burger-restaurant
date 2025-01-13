9f5f4d1555d6a4649323b878959992f6d4242136
Daniil Belogur <katokinawa@icloud.com>
2025-01-14 00:17:14 +0300

    feat: updated the application infrastructure and prepared the state management store.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/app/app'
 import './index.module.css'
+import { compose, createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { thunk } from 'redux-thunk';
+import { rootReducer } from './services/reducers';
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+const store = createStore(rootReducer, enhancer);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
---
+++ src/services/reducers/ingredients.js
@@ -0,0 +1,10 @@
+const initialState = {
+};
+
+export const ingredients = (state = initialState, action) => {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+}
